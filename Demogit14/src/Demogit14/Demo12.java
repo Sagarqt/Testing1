0d92fbabe9ec39138f51f023cf51c0efa2350768
Admin <Admin@DESKTOP-MBKU2UJ>
2022-02-11 19:05:04 +0530

    by Admin

---
+++ Demogit14/src/Demogit14/Demo12.java
@@ -0,0 +1,14 @@
+package Demogit14;
+
+public class Demo12 {
+
+	
+	
+	public void m2()
+	{
+		System.out.println("m2");
+	}
+	public static void main(String[] args) {
+		System.out.println("MAIN HII");
+	}
+}
